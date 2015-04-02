e19d1c2ca333defc79e719e09ff120b8050f6dfa
bschron <brunochroniaris@outlook.com>
2015-04-02 11:14:45 -0300

    created BillingHandlerDelegate

---
+++ Bigu/BillingHandlerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  BillingHandlerDelegate.swift
+//  Bigu
+//
+//  Created by Bruno Chroniaris on 4/2/15.
+//  Copyright (c) 2015 Universidade Federal De Alagoas. All rights reserved.
+//
+
+import Foundation
+
+protocol BillingHandlerDelegate {
+    func updateBillingUI()
+}
