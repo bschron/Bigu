33897b337a28ffff2b442d5aabcdff5d9811a397
bschron <brunochroniaris@outlook.com>
2015-04-14 12:33:52 -0300

    added some animations

--- Bigu/UserCell.swift
+++ Bigu/UserCell.swift
@@ -27,6 +27,7 @@ class UserCell: UITableViewCell {
             self.user = User.usersList.list[self.userIndex]
         }
     }
+    private var originalColor: UIColor?
     
     //MARK: Outlets
     @IBOutlet weak var nameLabel: UILabel!
@@ -43,7 +44,25 @@ class UserCell: UITableViewCell {
     
     func handleSwipes(sender: UISwipeGestureRecognizer){
         if sender.direction == .Right{
-            viewController.performSegueWithIdentifier(UsersViewController.userDetailSegueIdentifier, sender: self)
+            let duration = 0.25
+            let delay = 0.0 // delay will be 0.0 seconds (e.g. nothing)
+            let options = UIViewAnimationOptions.CurveEaseInOut
+            let width = self.frame.size.width
+            
+            UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                self.backgroundColor = UIColor(red: CGFloat(0.2), green: CGFloat(0.59607843), blue: CGFloat(0.85882353), alpha: CGFloat(1))
+                self.center.x += width
+                self.frame.size.width = CGFloat(0)
+                }, completion: { finished in
+                    
+                    UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                        self.backgroundColor = UIColor.whiteColor()
+                        self.frame.size.width = width
+                        self.center.x -= width
+                        }, completion: { finished in
+                            User.usersList.list[self.userIndex].resetBalance()
+                    })
+            })
         }
     }
     
@@ -59,6 +78,8 @@ class UserCell: UITableViewCell {
         let userImageViewWidth = self.userImageView.frame.width
         self.userImageView.layer.cornerRadius = userImageViewWidth / 2
         self.userImageView.layer.masksToBounds = true
+        
+        self.originalColor = self.backgroundColor
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -80,6 +101,27 @@ class UserCell: UITableViewCell {
     @IBAction func debitButtonPressed(sender: AnyObject) {
         let currentTaxValue = TaxCell.taxValue
         User.usersList.list[self.userIndex].debitValue(currentTaxValue)
+        
+        let duration = 0.25
+        let delay = 0.0 // delay will be 0.0 seconds (e.g. nothing)
+        let options = UIViewAnimationOptions.CurveEaseInOut
+        let width = self.frame.size.width
+        
+        UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+            self.backgroundColor = UIColor.greenColor().colorWithAlphaComponent(0.5)
+            self.center.x += width
+            self.frame.size.width = CGFloat(0)
+            }, completion: { finished in
+                
+                UIView.animateWithDuration(duration, delay: delay, options: options, animations: {
+                    self.backgroundColor = UIColor.whiteColor()
+                    self.frame.size.width = width
+                    self.center.x -= width
+                    }, completion: { finished in
+                        // any code entered here will be applied
+                        // once the animation has completed
+                })
+        })
     }
     
     // MARK: - Class Methods and Properties
