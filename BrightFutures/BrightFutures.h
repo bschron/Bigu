b75fa681df402416d7a768a99decb916260ff50d
bschron <brunochroniaris@outlook.com>
2015-04-24 16:09:04 -0300

    Modularized BrightFutures

---
+++ BrightFutures/BrightFutures.h
@@ -0,0 +1,19 @@
+//
+//  BrightFutures.h
+//  BrightFutures
+//
+//  Created by Bruno Chroniaris on 4/24/15.
+//  Copyright (c) 2015 Universidade Federal De Alagoas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrightFutures.
+FOUNDATION_EXPORT double BrightFuturesVersionNumber;
+
+//! Project version string for BrightFutures.
+FOUNDATION_EXPORT const unsigned char BrightFuturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrightFutures/PublicHeader.h>
+
+
