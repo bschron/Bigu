558c9ab05878b99f40f4b4d93b0f13c528f44e00
bschron <brunochroniaris@outlook.com>
2015-04-24 18:35:19 -0300

    modularised whole app as MVC

---
+++ DarkThemedViewController/DarkThemedViewController.h
@@ -0,0 +1,19 @@
+//
+//  DarkThemedViewController.h
+//  DarkThemedViewController
+//
+//  Created by Bruno Chroniaris on 4/24/15.
+//  Copyright (c) 2015 Universidade Federal De Alagoas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DarkThemedViewController.
+FOUNDATION_EXPORT double DarkThemedViewControllerVersionNumber;
+
+//! Project version string for DarkThemedViewController.
+FOUNDATION_EXPORT const unsigned char DarkThemedViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarkThemedViewController/PublicHeader.h>
+
+
