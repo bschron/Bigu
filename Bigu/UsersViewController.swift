665ad14cfea9d5313c4561f95d494e203470476e
bschron <brunochroniaris@outlook.com>
2015-05-09 23:59:20 -0300

    implemented AddressBookImportAgent

--- Bigu/UsersViewController.swift
+++ Bigu/UsersViewController.swift
@@ -18,11 +18,10 @@ import BrightFutures
 import ExpectedError
 import ABImportAgent
 
-public class UsersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UserHandlingDelegate, ABPeoplePickerNavigationControllerDelegate {
+public class UsersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UserHandlingDelegate {
 
     // MARK: - Proprierties
     private weak var popUp: NewUserPopUp?
-    internal var peoplePicker: ABPeoplePickerNavigationController?
     private var agent: ABImportAgent?
     
     //MARK: Outlets
@@ -60,13 +59,6 @@ public class UsersViewController: UIViewController, UITableViewDataSource, UITab
         self.agent?.displayPeoplePicker(viewController: self, onKill: {}, completion: {
             self.freePopUp()
         })
-    }
-    
-    public func didRegisterUser() {
-        let rows = UserList.sharedUserList.list.count - 1
-        let index = NSIndexPath(forRow: rows, inSection: 0)
-        self.usersTableView.insertRowsAtIndexPaths([index], withRowAnimation: .Automatic)
-        self.usersTableView.reloadData()
     }
     
     // MARK: Actions
@@ -156,124 +148,19 @@ public class UsersViewController: UIViewController, UITableViewDataSource, UITab
         self.performSegueWithIdentifier(UsersViewController.userDetailSegueIdentifier, sender: cell)
     }
     
-    // MARK: ABPeoplePickerNavigationControllerDelegate
-    public func peoplePickerNavigationControllerDidCancel(peoplePicker: ABPeoplePickerNavigationController!) {
-    }
-    public func peoplePickerNavigationController(peoplePicker: ABPeoplePickerNavigationController!, didSelectPerson person: ABRecord!) {
-        
-        func registerUser(user newUser: AbstractUser) {
-            let usr = newUser as! User
-            UserList.sharedUserList.insertUser(usr)
-            self.usersTableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.usersTableView.numberOfRowsInSection(0), inSection: 0)], withRowAnimation: .Automatic)
-        }
-        
-        self.popUp?.terminate()
-        
-        let futureAddress = UsersViewController.peekAddress(viewController: self, person: person)
-        let registerFunction: (AbstractUser) -> () = registerUser
-        
-        let futureUsr = User.loadUserFromAddressBook(viewController: self, person: person, address: futureAddress)
-        futureUsr.onSuccess{ newUser in
-            registerFunction(newUser)
-        }.onFailure{ error in
-            let err = error as! ExpectedError
-            
-            if err.id == NewUserError.codeSet.hasNoAddress.rawValue || err.id == NewUserError.codeSet.choseNoAddress.rawValue{
-                let newUser = error.valueForKey("newUser") as! AbstractUser
-                registerFunction(newUser)
-            }
-            else if err.id == NewUserError.codeSet.couldNotFindLocation.rawValue {
-                let newUser = error.valueForKey("newUser") as! AbstractUser
-                registerFunction(newUser)
-                
-                let alert = UIAlertController(title: "Could not find location", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-                let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
-                alert.addAction(action)
-                self.presentViewController(alert, animated: true, completion: nil)
-            }
-            else {
-                let alert = UIAlertController(title: "No Result for this address", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
-                let action = UIAlertAction(title: "Done", style: UIAlertActionStyle.Default, handler: nil)
-                alert.addAction(action)
-                self.presentViewController(alert, animated: true, completion: nil)
-            }
-        }
-    }
-    
     // MARK: UserHandlingDelegate
     public func reloadUsersData() {
         self.usersTableView.reloadData()
     }
     
+    public func didRegisterUser() {
+        let rows = UserList.sharedUserList.list.count - 1
+        let index = NSIndexPath(forRow: rows, inSection: 0)
+        self.usersTableView.insertRowsAtIndexPaths([index], withRowAnimation: .Automatic)
+    }
+    
     // MARK: - Class Properties
     class var userDetailSegueIdentifier: String {
         return "UserDetailSegue"
-    }
-    class private func peekAddress(viewController vc: UIViewController, person: ABRecord!) -> Future<NSDictionary> {
-        
-        let addresses: ABMultiValueRef? = ABRecordCopyValue(person, kABPersonAddressProperty)?.takeRetainedValue()
-        let count = ABMultiValueGetCount(addresses)
-        
-        let promise = Promise<NSDictionary>()
-        
-        if count == 0 {
-            promise.failure(NewUserError(description: "user has no address", id: NewUserError.codeSet.hasNoAddress.rawValue))
-            return promise.future
-        }
-        
-        let alert = UIAlertController(title: "Select a home address", message: "Select the address to attribute to this profile", preferredStyle: UIAlertControllerStyle.ActionSheet)
-        
-        for var i = 0; i < count; i++ {
-            let add: NSDictionary! = ABMultiValueCopyValueAtIndex(addresses, i).takeRetainedValue() as? NSDictionary
-            
-            if add == nil {
-                promise.failure(NewUserError(description: "user has no address", id: NewUserError.codeSet.hasNoAddress.rawValue))
-                return promise.future
-            }
-            
-            let optionalCountry = add[kABPersonAddressCountryKey as! String] as? String
-            let optionalCity = add[kABPersonAddressCityKey as! String] as? String
-            let optionalState = add[kABPersonAddressStateKey as! String] as? String
-            let optionalZip = add[kABPersonAddressZIPKey as! String] as? String
-            let optionalStreet = add[kABPersonAddressStreetKey as! String] as? String
-            
-            var addressPresentation = ""
-            
-            if let cur = optionalStreet {
-                addressPresentation += cur
-            }
-            else if let cur = optionalCity {
-                addressPresentation += cur
-            }
-            else if let cur = optionalZip {
-                addressPresentation += cur
-            }
-            else if let cur = optionalState {
-                addressPresentation += cur
-            }
-            else if let cur = optionalCountry {
-                addressPresentation += cur
-            }
-            
-            let action = UIAlertAction(title: addressPresentation, style: UIAlertActionStyle.Default, handler: {action in
-                promise.success(add!)
-            })
-            
-            alert.addAction(action)
-        }
-        
-        let cancelAction = UIAlertAction(title: "Don't Attribute address", style: UIAlertActionStyle.Cancel, handler: { action in
-            promise.failure(NewUserError(description: "User chose not to register address", id: NewUserError.codeSet.choseNoAddress.rawValue))
-        })
-        
-        alert.addAction(cancelAction)
-        
-        let delayTime = dispatch_time(DISPATCH_TIME_NOW,
-            Int64(1 * Double(NSEC_PER_SEC)))
-        dispatch_after(delayTime, dispatch_get_main_queue(), {
-            vc.presentViewController(alert, animated: true, completion: nil)
-        })
-        
-        return promise.future
     }
 }
